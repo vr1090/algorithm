9c3d0183b7591c99bf2a671b480ac4ab6ec66198
inu <vr1090@gmail.com>
2015-01-31 22:20:29 +0700

    nom nom nom

--- src/Percolation.java
+++ src/Percolation.java
@@ -11,13 +11,13 @@ public class Percolation {
 	private int rootAtas = 0;
 	private int rootBawah = -1;
 
-	private UF guardian = null;
+	private QuickUnionUF guardian = null;
 
 	// create N-by-N grid, with all sites blocked
 	public Percolation(int n) {
 		// System.out.println("hei");
 		blocks = new Block[n + 1][n + 1];
-		guardian = new UF(n * n + 2);
+		guardian = new QuickUnionUF(n * n + 2);
 		int pos = 1;
 		// initialize dulu kali yah..
 		for (int row = 1; row <= n; row++) {
@@ -146,60 +146,7 @@ public class Percolation {
 		int arrayPos;
 	}// end of private class
 
-	private class UF {
-
-		private int[] master = null;
-		private int[] size = null;
-		private int panjang = 0;
-
-		public UF(int N) {
-			master = new int[N];
-			size = new int[N];
-			panjang = N;
-
-			// init as usual
-			for (int i = 0; i < N; i++) {
-				master[i] = i;
-				size[i] = 1;
-			}// end for
-		}// end of constructor
-
-		private int findRoot(int node) {
-			while (node != master[node]) {
-				// master[node]= master[master[node]];
-				node = master[node];
-			}// end while
-
-			return node;
-		}// end of method
-
-		public void union(int p, int q) {
-			int rootP = findRoot(p);
-			int rootQ = findRoot(q);
-
-			if (size[rootP] < size[rootQ]) {
-				master[rootP] = rootQ;
-				size[rootQ] = size[rootQ] + size[rootP];
-			}// end if
-			else {
-				master[rootQ] = rootP;
-				size[rootP] = size[rootQ] + size[rootP];
-
-			}// end else
-
-		}// end of method
-
-		public boolean connected(int p, int q) {
-			return findRoot(p) == findRoot(q);
-		}// end of method
-
-		public void print() {
-			System.out.println(Arrays.toString(master));
-		}// end of method
-
-	}// end of class
-
-	public static void main(String[] args) {
+		public static void main(String[] args) {
 		Percolation per = new Percolation(3);
 		// per.getUF().print();
 		per.open(1, 3);
