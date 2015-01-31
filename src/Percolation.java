405e367020e15f8713985a29f1e95acd7c03ef0a
inu <vr1090@gmail.com>
2015-01-31 22:12:09 +0700

    nom nom nom

---
+++ src/Percolation.java
@@ -0,0 +1,216 @@
+
+import java.util.Arrays;
+
+public class Percolation {
+	private Block[][] blocks = null;
+	private int minX = 1;
+	private int minY = 1;
+	private int maxX = -1;
+	private int maxY = -1;
+
+	private int rootAtas = 0;
+	private int rootBawah = -1;
+
+	private UF guardian = null;
+
+	// create N-by-N grid, with all sites blocked
+	public Percolation(int n) {
+		// System.out.println("hei");
+		blocks = new Block[n + 1][n + 1];
+		guardian = new UF(n * n + 2);
+		int pos = 1;
+		// initialize dulu kali yah..
+		for (int row = 1; row <= n; row++) {
+
+			for (int col = 1; col <= n; col++) {
+				Block newBlock = new Block();
+				newBlock.isOpen = false;
+				newBlock.x = row;
+				newBlock.y = col;
+				newBlock.arrayPos = pos;
+
+				blocks[row][col] = newBlock;
+				pos++;
+			}// end for
+		}// end for
+
+		// set the first row
+		// for( int i=1; i<=n;i++)
+		// {
+		// guardian.union(0, i);
+		// }//end of for
+
+		int last = (n * n) + 1;
+
+		// for(int i=last-n; i<last;i++)
+		// guardian.union(last, i);
+
+		// sing yang dibutuhken ...
+		maxX = n;
+		maxY = n;
+		rootBawah = last;
+
+		// System.out.println("finish");
+	}// end of method
+
+	private boolean open1(int x, int y) {
+		// System.out.println("open "+ x +" "+y);
+		Block b = blocks[x][y];
+		if (b.isOpen)
+			return false;
+
+		open(x, y);
+		return true;
+	}// end of method
+
+	// open site (row i, column j) if it is not open already
+	public void open(int x, int y) {
+		Block b = blocks[x][y];
+
+		if (b.isOpen)
+			return;
+
+		b.isOpen = true;
+
+		// kanan kiri atas bawah om
+		int xKiri = x - 1;
+		int xKanan = x + 1;
+		int yAtas = y - 1;
+		int yBawah = y + 1;
+
+		// cek kiri
+		if (xKiri >= minX) {
+			Block bxkiri = blocks[xKiri][y];
+			if (bxkiri.isOpen) {
+				guardian.union(b.arrayPos, bxkiri.arrayPos);
+			}// end of if
+		}// end if
+
+		// cek kanan
+		if (xKanan <= maxX) {
+			Block bTemp = blocks[xKanan][y];
+			if (bTemp.isOpen) {
+				guardian.union(b.arrayPos, bTemp.arrayPos);
+			}// end of if
+		}// end if
+
+		// cek atas
+		if (yAtas >= minY) {
+			Block bTemp = blocks[x][yAtas];
+			if (bTemp.isOpen) {
+				guardian.union(b.arrayPos, bTemp.arrayPos);
+			}// end of if
+		}// end if
+
+		// cek bawah
+		if (yBawah <= maxY) {
+			Block bTemp = blocks[x][yBawah];
+			if (bTemp.isOpen) {
+				guardian.union(b.arrayPos, bTemp.arrayPos);
+			}// end of if
+		}// end if
+
+		if (x == minY)
+			guardian.union(rootAtas, b.arrayPos);
+
+		if (!this.percolates() && x == maxY)
+			guardian.union(rootBawah, b.arrayPos);
+	}// end of method
+
+	// is site (row i, column j) open?
+	public boolean isOpen(int i, int j) {
+		return (blocks[i][j]).isOpen;
+	}
+
+	// is site (row i, column j) full?
+	public boolean isFull(int i, int j) {
+		Block b = blocks[i][j];
+
+		if (!b.isOpen)
+			return false;
+		//System.out.println(i + "," + j + "," + b.arrayPos);
+		//guardian.print();
+		return guardian.connected(rootAtas, b.arrayPos);
+	}
+
+	// does the system percolate?x
+	public boolean percolates() {
+
+		return guardian.connected(rootAtas, rootBawah);
+	}// end of method
+
+	private class Block {
+		int x = -1;
+		int y = -1;
+		boolean isOpen = false;
+		int arrayPos;
+	}// end of private class
+
+	private class UF {
+
+		private int[] master = null;
+		private int[] size = null;
+		private int panjang = 0;
+
+		public UF(int N) {
+			master = new int[N];
+			size = new int[N];
+			panjang = N;
+
+			// init as usual
+			for (int i = 0; i < N; i++) {
+				master[i] = i;
+				size[i] = 1;
+			}// end for
+		}// end of constructor
+
+		private int findRoot(int node) {
+			while (node != master[node]) {
+				// master[node]= master[master[node]];
+				node = master[node];
+			}// end while
+
+			return node;
+		}// end of method
+
+		public void union(int p, int q) {
+			int rootP = findRoot(p);
+			int rootQ = findRoot(q);
+
+			if (size[rootP] < size[rootQ]) {
+				master[rootP] = rootQ;
+				size[rootQ] = size[rootQ] + size[rootP];
+			}// end if
+			else {
+				master[rootQ] = rootP;
+				size[rootP] = size[rootQ] + size[rootP];
+
+			}// end else
+
+		}// end of method
+
+		public boolean connected(int p, int q) {
+			return findRoot(p) == findRoot(q);
+		}// end of method
+
+		public void print() {
+			System.out.println(Arrays.toString(master));
+		}// end of method
+
+	}// end of class
+
+	public static void main(String[] args) {
+		Percolation per = new Percolation(3);
+		// per.getUF().print();
+		per.open(1, 3);
+		per.open(2, 3);
+		per.open(3, 3);
+		per.open(3, 1);
+		// System.out.println( per.isFull(1, 2));
+		System.out.println(per.percolates());
+		System.out.println("is full .. " + per.isFull(3, 1));
+		// per.getUF().print();
+
+	}// end of method
+
+}// end of class
---
+++ src/PercolationStats.java
@@ -0,0 +1,87 @@
+
+import java.util.*;
+
+public class PercolationStats {
+	private int dimension = 0;
+	private int count_montecarlo = 0;
+	private List<Double> result;
+	private double mean;
+	private double stdev;
+
+	public PercolationStats(int N, int T) // perform T independent experiments
+											// on an N-by-N grid
+	{
+		dimension = N;
+		count_montecarlo = T;
+		result = new ArrayList<Double>();
+
+		for (int i = 0; i < count_montecarlo; i++) {
+
+			Percolation quick = new Percolation(N);
+			int attempt = 0;
+
+			while (!quick.percolates()) {
+				int x = (int) ((Math.random() * dimension) + 1);
+				int y = (int) ((Math.random() * dimension) + 1);
+				// System.out.println( "open ("+x + ","+y+")");
+				// quick.open(x,y);
+				// System.out.println(quick.percolates() );
+				if (quick.isOpen(x, y))
+					continue;
+				attempt++;
+			}// end of while
+
+			result.add((double) attempt / (dimension * dimension));
+
+		}// end for
+
+	}// end of method
+
+	public double mean() // sample mean of percolation threshold
+	{
+		double tempValue = 0;
+
+		for (Double d : result) {
+			tempValue = tempValue + d;
+		}// end for
+		mean = tempValue / count_montecarlo;
+		return mean;
+	}// end of method
+
+	public double stddev() // sample standard deviation of percolation threshold
+	{
+		double tempValue = 0;
+
+		for (Double d : result) {
+			tempValue = tempValue + Math.pow((d - mean), 2);
+		}// end for
+
+		stdev = tempValue / (count_montecarlo - 1);
+
+		return stdev;
+	}// end of method
+
+	public double confidenceLo() // low endpoint of 95% confidence interval
+	{
+		return mean - 1.96 * Math.sqrt(stdev) / Math.sqrt(count_montecarlo);
+	}// end of method
+
+	public double confidenceHi() // high endpoint of 95% confidence interval
+	{
+		return mean + 1.96 * Math.sqrt(stdev) / Math.sqrt(count_montecarlo);
+	}// end of method
+
+	public static void main(String[] args) // test client (described below)
+	{
+		// bikin UI dulu ah kaka
+		int n = Integer.parseInt(args[0]);
+		int t = Integer.parseInt(args[1]);
+		PercolationStats stats = new PercolationStats(n, t);
+		System.out.println(String.format("%-24s = %f", "mean", stats.mean()));
+		System.out
+				.println(String.format("%-24s = %f", "stddev", stats.stddev()));
+		System.out.println(String.format("%-24s = %f, %f",
+				"95% confidence interval ", stats.confidenceLo(),
+				stats.confidenceHi()));
+	}// end of method
+}// end of class
